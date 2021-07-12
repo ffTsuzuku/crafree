a5df3d1947bddc78d7cef470742c940af90d3191
ffTsuzuku <gagandeepsandhu@protonmail.com>
2021-07-11 20:38:48 -0400

    Added packages and componentA

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react"
+import ComponentA from './components/componentA'
 
 const App = () => (
     <div>
         <h1>Hello React</h1>
+        <ComponentA />
     </div>
 )
 
---
+++ src/components/componentA/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './styles.css'
+
+export default function() {
+    return <div className={styles.card}>
+        <div className={styles.blueText}>Hello World</div>
+    </div>
+}
