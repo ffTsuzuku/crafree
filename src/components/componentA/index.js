2a28daeb4332ffc68f50883ca54f67c42fa9150b
ffTsuzuku <gagandeepsandhu@protonmail.com>
2021-07-12 04:09:44 -0400

    Gave ComponentA styles the proper filename

--- src/components/componentA/index.js
+++ src/components/componentA/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import styles from './styles.css'
+import styles from './ComponentA.module.css'
 
+console.log('Styles', styles)
 export default function() {
     return <div className={styles.card}>
-        <div className={styles.blueText}>Hello World</div>
+        <div className={styles.blueText}>Component A Styles</div>
     </div>
 }
